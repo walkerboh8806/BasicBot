33ccb20c3a1c1639ccecfb88eb30ccbf8d7b0eae
Sam Coddington <samc@wolfram.com>
2018-01-26 15:39:33 -0600

    Combined tournament_check and tournament_add_raw

--- smashgg.py
+++ smashgg.py
@@ -7,46 +7,41 @@ import os
 
 # need a way to force remove and download data
 
-# checks that tournament is a real tournament with information
-# checks if there is already a directory in data with the same name
-def tournament_check(tournament_name):
+# checks if tournament exists at all
+# checks if tournament has already been added
+# creates directory for tournament, and dumps raw files
+def tournament_add_raw(tournament_name):
     url = "https://api.smash.gg/tournament/"+str(tournament_name)+"?expand[]=event"
     response = json.loads(requests.get(url).content.decode('utf-8'))
     #print(response)
     keys = list(response.keys())
     if "message" in keys:
         print("not found")
-        return "```Unable to find Tournament with name: " + str(tournament_name) + "```"
+        return "Unable to find Tournament with name: " + str(tournament_name)
     elif response['resultEntity'] == "tournament":
         if os.path.isdir("data/tournaments/" + str(tournament_name)):
-            return "```Tournament has already been added.```"
+            return "Tournament has already been added."
         directory = "/home/samc/Dropbox/PycharmProjects/discord-bot/data/tournaments/" + str(tournament_name)
         os.mkdir(directory)
         os.mkdir(directory + "/raw")
-        return "```TournamentID: "+str(response['result'])+" Found```"
+        t_json_raw = response
+        # print(json.loads(response.content.decode('utf-8')))
+        directory = "/home/samc/Dropbox/PycharmProjects/discord-bot/data/tournaments/" + str(tournament_name)
+        with(open(directory + "/raw/tournament.json", 'w+')) as f:
+            json.dump(t_json_raw, f)
+        eventids = get_eventids(t_json_raw)
+        for event in eventids:
+            event_json_raw = get_event_raw(event)
+            with(open(directory + "/raw/event" + str(event) + ".json", 'w+')) as f:
+                json.dump(event_json_raw, f)
+            groupids = get_groupids(event_json_raw)
+            for group in groupids:
+                group_json_raw = get_group_raw(group)
+                with(open(directory + "/raw/group_" + str(event) + "_" + str(group) + ".json", 'w+')) as f:
+                    json.dump(group_json_raw, f)
+        return ["TournamentID: "+str(response['result'])+" found and added."]
     else:
-        return "```Unknown error occurred, check log.```"
-
-# creates directory for tournament, and dumps raw files
-def tournament_add_raw(tournament_name):
-    url = "https://api.smash.gg/tournament/"+str(tournament_name)+"?expand[]=event"
-    response = requests.get(url)
-    t_json_raw = json.loads(response.content.decode('utf-8'))
-    #print(json.loads(response.content.decode('utf-8')))
-    directory = "/home/samc/Dropbox/PycharmProjects/discord-bot/data/tournaments/"+str(tournament_name)
-    with(open(directory+"/raw/tournament.json", 'w+')) as f:
-        json.dump(t_json_raw, f)
-    eventids = get_eventids(t_json_raw)
-    for event in eventids:
-        event_json_raw = get_event_raw(event)
-        with(open(directory+"/raw/event"+str(event)+".json", 'w+')) as f:
-            json.dump(event_json_raw, f)
-        groupids = get_groupids(event_json_raw)
-        for group in groupids:
-            group_json_raw = get_group_raw(group)
-            with(open(directory+"/raw/group_"+str(event)+ "_" + str(group) + ".json", 'w+')) as f:
-                json.dump(group_json_raw, f)
-    return "```Raw data added to database```"
+        return "Unknown error occurred, check log."
 
 
 # creates list of eventids from raw tournament json
