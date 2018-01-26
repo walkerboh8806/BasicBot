3b00970d0121ba2ab8a240ad95e78ec8e0fc6eaa
Sam Coddington <samc@wolfram.com>
2018-01-26 15:41:17 -0600

    added discordify and combine_discordify to format text.  Changed list_tournaments to use new discordify functions

--- functions.py
+++ functions.py
@@ -3,5 +3,18 @@ import subprocess, os
 def list_tournaments():
     dir = '/home/samc/Dropbox/PycharmProjects/discord-bot/data/tournaments'
     filenames = os.listdir(dir)
-    print(filenames)
-    return filenames
+    message = discordify(filenames)
+    return message
+
+def discordify(message):
+    base = "```"+"\r\n"
+    for item in message:
+        base = base + str(item) + "\r\n"
+    final = base + "\r\n" +"```"
+    return final
+
+def combine_discordify(messages):
+    final = ""
+    for message in messages:
+        final = final + message + "\r\n"
+    return final
