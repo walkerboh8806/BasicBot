6e665ab6e35fc955de5ffd0d168f0adbfa377202
samc <walkerboh8806@gmail.com>
2018-01-26 00:38:37 -0600

    added function to get list of files in tournament directory

--- functions.py
+++ functions.py
@@ -1,1 +1,7 @@
-import json
+import subprocess, os
+
+def list_tournaments():
+    dir = '/home/samc/Dropbox/PycharmProjects/discord-bot/data/tournaments'
+    filenames = os.listdir(dir)
+    print(filenames)
+    return filenames
