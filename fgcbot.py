24202fab70e74719bbdf9f8fc2032653f6df56ab
samc <walkerboh8806@gmail.com>
2018-01-26 00:38:20 -0600

    added commands for add, tournament list and commands list

--- fgcbot.py
+++ fgcbot.py
@@ -1,3 +1,5 @@
+#!/usr/python3
+
 # These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
 
 import discord
@@ -5,12 +7,12 @@ import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
-import functions
-import smashgg
+from smashgg import *
+from functions import *
 
 
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
-client = Bot(description="Basic Bot by Habchy#1665", command_prefix="^", pm_help = False)
+client = Bot(description="Basic Bot by Habchy#1665", command_prefix="-fgcbot ", pm_help = False)
 
 
 
@@ -19,39 +21,42 @@ client = Bot(description="Basic Bot by Habchy#1665", command_prefix="^", pm_help
 
 @client.event
 async def on_ready():
-	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
-	print('--------')
-	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
-	print('--------')
-	print('Use this link to invite {}:'.format(client.user.name))
-	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
-	print('--------')
-	print('Support Discord Server: https://discord.gg/FNNNgqb')
-	print('Github Link: https://github.com/Habchy/BasicBot')
-	print('--------')
-	print('You are running BasicBot v2.1') #Do not change this. This will really help us support you, if you need support.
-	print('Created by Habchy#1665')
-	return await client.change_presence(game=discord.Game(name='PLAYING STATUS HERE')) #This is buggy, let us know if it doesn't work.
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+    print('--------')
+    print('Use this link to invite {}:'.format(client.user.name))
+    print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
+    print('--------')
+    print('Support Discord Server: https://discord.gg/FNNNgqb')
+    print('Github Link: https://github.com/Habchy/BasicBot')
+    print('--------')
+    print('You are running BasicBot v2.1') #Do not change this. This will really help us support you, if you need support.
+    print('Created by Habchy#1665')
+    return await client.change_presence(game=discord.Game(name='TOURNAMENT STATS')) #This is buggy, let us know if it doesn't work.
 
 
 
 # This is a basic example of a call and response command. You tell it do "this" and it does it.
+commands = ["commands list", "add", "list"]
 
 @client.command()
-async def tournament(*args):
-    flag = args[0]
-#    if flag == "add":
-#        functions.tournament_add(str(args[1]))
-#        await client.say("Added tournament: **" + args[1] + "**")
-#    elif flag == "remove":
-#        functions.tournament_remove(tournament)
-#        await client.say("Removed tournament: **" + args[1] + "**")
-    if flag == "list":
-        await client.say(functions.tournament_list())
-    elif flag =="results":
-        eventids = functions.get_eventids(args[1])
-        groupids = functions.get_groupids(eventids)
-        results = functions.get_player_standing(groupids)
+async def commands(*args):
+    if args[0] == "list":
+        await client.say("```add: add tournamnet raw data to database."+"\r\n"+"usage: -fgcbot add <tournament_name>"+"\r\n"+"\r\n"+"list: list all tournaments in database."+"\r\n"+"usage: -fgcbot list```")
+
+@client.command()
+async def list(*args):
+    await client.say(list_tournaments())
+
+@client.command()
+async def add(*args):
+    await client.say("Give me some time if it's a large tournament...")
+    check = tournament_check(args[0])
+    add = tournament_add_raw(args[0])
+    message =check+"\r\n"+add
+    await client.say(message)
+
 #async def tournament(*args):
 
 
